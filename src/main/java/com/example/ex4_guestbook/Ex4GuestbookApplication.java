6fb281b978793d89f4a3883d7228515ef02caa6b
ImKunYoung <lim8667@gmail.com>
2022-07-06 05:46:29 +0900

    first commit

---
+++ src/main/java/com/example/ex4_guestbook/Ex4GuestbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex4_guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex4GuestbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex4GuestbookApplication.class, args);
+    }
+
+}
