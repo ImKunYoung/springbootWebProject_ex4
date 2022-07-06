8dd8b6e38b0709d60c592c0f1bcaec0aede27fb3
ImKunYoung <lim8667@gmail.com>
2022-07-06 10:54:54 +0900

    자동으로 처리되는 날짜/시간 설정

--- src/main/java/com/example/ex4_guestbook/Ex4GuestbookApplication.java
+++ src/main/java/com/example/ex4_guestbook/Ex4GuestbookApplication.java
@@ -2,8 +2,10 @@ package com.example.ex4_guestbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing /*AuditingEntityListener 활성화를 위함*/
 public class Ex4GuestbookApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/ex4_guestbook/controller/GuestbookController.java
+++ src/main/java/com/example/ex4_guestbook/controller/GuestbookController.java
@@ -1,4 +1,4 @@
-package com.example.ex4_guestbook.Controller;
+package com.example.ex4_guestbook.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
