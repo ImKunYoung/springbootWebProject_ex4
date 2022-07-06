afafec41fb8873625a599aeba94053cca4a4dbd8
ImKunYoung <lim8667@gmail.com>
2022-07-06 17:29:33 +0900

    게시판 제목, 내용, 업데이트 시간 수정 테스트

--- src/test/java/com/example/ex4_guestbook/repository/GuestBookRepositoryTests.java
+++ src/test/java/com/example/ex4_guestbook/repository/GuestBookRepositoryTests.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.example.ex4_guestbook.entity.Guestbook;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -27,4 +28,24 @@ public class GuestBookRepositoryTests {
             System.out.println(guestbookRepository.save(guestbook));
         });
     }
+
+    /*수정 시간 test*/
+    @Test
+    public void updateTest() {
+
+        Optional<Guestbook> result = guestbookRepository.findById(300L);
+
+        if(result.isPresent()) {
+
+            Guestbook guestbook = result.get();
+
+            guestbook.changeTitle("Change Title....");
+            guestbook.changeContent("Change Content....");
+
+            guestbookRepository.save(guestbook);
+
+        }
+
+    }
+
 }
