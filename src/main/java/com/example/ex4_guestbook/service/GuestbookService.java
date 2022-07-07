0cb18364cf7480396831ac04386f0f3fef9a45ad
ImKunYoung <lim8667@gmail.com>
2022-07-08 07:50:59 +0900

    목록 처리 -1-3 서비스 계층에서의 목록 처리

--- src/main/java/com/example/ex4_guestbook/service/GuestbookService.java
+++ src/main/java/com/example/ex4_guestbook/service/GuestbookService.java
@@ -1,11 +1,15 @@
 package com.example.ex4_guestbook.service;
 
 import com.example.ex4_guestbook.dto.GuestbookDTO;
+import com.example.ex4_guestbook.dto.PageRequestDTO;
+import com.example.ex4_guestbook.dto.PageResultDTO;
 import com.example.ex4_guestbook.entity.Guestbook;
 
 public interface GuestbookService {
     /*방명록을 등록한다*/
     Long register(GuestbookDTO dto);
+
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
 
     /*등록과 DTO 를 엔티티로 변환하기*/
     default Guestbook dtoToEntity(GuestbookDTO dto) {
@@ -16,5 +20,18 @@ public interface GuestbookService {
                 .writer(dto.getWriter())
                 .build();
         return entity;
+    }
+    /*엔티티를 DTO 로 반환*/
+    default GuestbookDTO entityToDto(Guestbook entity) {
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+
+        return dto;
     }
 }
--- src/main/java/com/example/ex4_guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/com/example/ex4_guestbook/service/GuestbookServiceImpl.java
@@ -1,11 +1,19 @@
 package com.example.ex4_guestbook.service;
 
 import com.example.ex4_guestbook.dto.GuestbookDTO;
+import com.example.ex4_guestbook.dto.PageRequestDTO;
+import com.example.ex4_guestbook.dto.PageResultDTO;
 import com.example.ex4_guestbook.entity.Guestbook;
 import com.example.ex4_guestbook.repository.GuestbookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
 
 @Service /*스프링에서 빈으로 처리하기 위함*/
 @Log4j2
@@ -27,5 +35,18 @@ public class GuestbookServiceImpl implements GuestbookService {
 
         return entity.getGno();
     }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+
+        Page<Guestbook> result = repository.findAll(pageable);
+
+        Function<Guestbook, GuestbookDTO> fn = (this::entityToDto);
+
+        return new PageResultDTO<>(result, fn);
+
+    }
+
 
 }
