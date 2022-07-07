9a79967b836873eb13114c6dac46fa4f22505cf5
ImKunYoung <lim8667@gmail.com>
2022-07-07 09:58:33 +0900

    등록과 DTO 를 엔티티로 변환하기 (dto -> entity, entity -> dto)

--- src/main/java/com/example/ex4_guestbook/service/GuestbookService.java
+++ src/main/java/com/example/ex4_guestbook/service/GuestbookService.java
@@ -1,8 +1,20 @@
 package com.example.ex4_guestbook.service;
 
 import com.example.ex4_guestbook.dto.GuestbookDTO;
+import com.example.ex4_guestbook.entity.Guestbook;
 
 public interface GuestbookService {
     /*방명록을 등록한다*/
     Long register(GuestbookDTO dto);
+
+    /*등록과 DTO 를 엔티티로 변환하기*/
+    default Guestbook dtoToEntity(GuestbookDTO dto) {
+        Guestbook entity = Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
 }
