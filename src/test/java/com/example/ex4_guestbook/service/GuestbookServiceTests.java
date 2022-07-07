95a03e0608f4c34d419faed4ee6b019e787dbb08
ImKunYoung <lim8667@gmail.com>
2022-07-07 10:06:24 +0900

    (서비스 계층의 테스트) 등록과 DTO 를 엔티티로 변환하기

---
+++ src/test/java/com/example/ex4_guestbook/service/GuestbookServiceTests.java
@@ -0,0 +1,27 @@
+package com.example.ex4_guestbook.service;
+
+import com.example.ex4_guestbook.dto.GuestbookDTO;
+import com.example.ex4_guestbook.entity.Guestbook;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GuestbookServiceTests {
+
+    @Autowired
+    private GuestbookService service;
+
+    @Test
+    public void testRegister() {
+
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .title("Sample Title...")
+                .content("Sample Content...")
+                .writer("user0")
+                .build();
+
+        System.out.println(service.register(guestbookDTO));
+
+    }
+}
