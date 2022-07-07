146afd7686cccde1460863ad4118cefbc3eaf9f2
ImKunYoung <lim8667@gmail.com>
2022-07-07 10:00:53 +0900

    등록과 DTO 를 엔티티로 변환하기 (dto -> entity, entity -> dto)

--- src/main/java/com/example/ex4_guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/com/example/ex4_guestbook/service/GuestbookServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.ex4_guestbook.service;
 
 import com.example.ex4_guestbook.dto.GuestbookDTO;
+import com.example.ex4_guestbook.entity.Guestbook;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,15 @@ public class GuestbookServiceImpl implements GuestbookService {
 
     @Override
     public Long register(GuestbookDTO dto) {
+        log.info("DTO-------------------------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+
+        log.info(entity);
+
         return null;
     }
+
+
 }
