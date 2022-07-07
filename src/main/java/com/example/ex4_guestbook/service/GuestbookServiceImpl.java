285d5a284a5fad571045f4a6f3ac14df320a515c
ImKunYoung <lim8667@gmail.com>
2022-07-07 10:10:37 +0900

    (서비스 계층의 테스트) 등록과 DTO 를 엔티티로 변환하기 - 테스트 결과 DB save

--- src/main/java/com/example/ex4_guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/com/example/ex4_guestbook/service/GuestbookServiceImpl.java
@@ -2,12 +2,17 @@ package com.example.ex4_guestbook.service;
 
 import com.example.ex4_guestbook.dto.GuestbookDTO;
 import com.example.ex4_guestbook.entity.Guestbook;
+import com.example.ex4_guestbook.repository.GuestbookRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service /*스프링에서 빈으로 처리하기 위함*/
 @Log4j2
+@RequiredArgsConstructor /*의존성 자동 주입*/
 public class GuestbookServiceImpl implements GuestbookService {
+
+    private final GuestbookRepository repository; /*반드시 final 로 선언*/
 
     @Override
     public Long register(GuestbookDTO dto) {
@@ -18,8 +23,9 @@ public class GuestbookServiceImpl implements GuestbookService {
 
         log.info(entity);
 
-        return null;
-    }
+        repository.save(entity);
 
+        return entity.getGno();
+    }
 
 }
