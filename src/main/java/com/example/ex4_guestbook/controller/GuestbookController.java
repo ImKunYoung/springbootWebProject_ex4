e5014f3d88de932c6634b3fccafc8f7ee0ea7ac3
ImKunYoung <lim8667@gmail.com>
2022-07-09 06:36:59 +0900

    컨트롤러와 화면에서의 목록 처리 - 스프링 MVC 파라미터를 자동으로 수집. - http://localhost:8080/guestbook/list?size=19&page=4 <= 이렇게 하면 지알아서 감

--- src/main/java/com/example/ex4_guestbook/controller/GuestbookController.java
+++ src/main/java/com/example/ex4_guestbook/controller/GuestbookController.java
@@ -1,21 +1,36 @@
 package com.example.ex4_guestbook.controller;
 
+import com.example.ex4_guestbook.dto.PageRequestDTO;
+import com.example.ex4_guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor // 자동 주입을 위한 Annotation
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
+    private final GuestbookService service;
+
+    @GetMapping({"/"})
     public String list() {
 
         log.info("list............");
 
-        return "/guestbook/list"; // /guestbook/list 로 매핑
+        return "redirect:/guestbook/list"; // /guestbook/list 로 매핑
+
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+
+        log.info("list............"+pageRequestDTO);
+        model.addAttribute("result", service.getList(pageRequestDTO));
 
     }
 
