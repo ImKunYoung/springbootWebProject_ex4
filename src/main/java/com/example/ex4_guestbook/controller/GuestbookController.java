355d429dc022b7cbd3b8a23692a20d559aa62a86
ImKunYoung <lim8667@gmail.com>
2022-07-10 13:30:04 +0900

    등록 페이지와 등록 처리

--- src/main/java/com/example/ex4_guestbook/controller/GuestbookController.java
+++ src/main/java/com/example/ex4_guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.example.ex4_guestbook.controller;
 
+import com.example.ex4_guestbook.dto.GuestbookDTO;
 import com.example.ex4_guestbook.dto.PageRequestDTO;
 import com.example.ex4_guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -32,6 +35,26 @@ public class GuestbookController {
         log.info("list............"+pageRequestDTO);
         model.addAttribute("result", service.getList(pageRequestDTO));
 
+    }
+
+    /*등록 페이지와 등록 처리 - 등록 페이지*/
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get...");
+    }
+    /*등록 페이지와 등록 처리 - 등록 처리*/
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+
+        log.info("dto..."+dto);
+
+        // 새로 추가된 엔티티의 번호
+        Long gno = service.register(dto);
+
+        // 단 한 번만 데이터를 전달하는 용도
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
     }
 
 }
